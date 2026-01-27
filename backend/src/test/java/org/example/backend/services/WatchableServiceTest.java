1c45d5a53f579b9b4b6857171b437c16e7ce60fb
DenizWassmuth <deniz-wassmuth@gmx.de>
2026-01-27 15:21:14 +0100

    add WatchableServiceTest for getAll

---
+++ backend/src/test/java/org/example/backend/services/WatchableServiceTest.java
@@ -0,0 +1,57 @@
+package org.example.backend.services;
+
+import org.example.backend.models.Watchable;
+import org.example.backend.repos.WatchableRepo;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class WatchableServiceTest {
+
+//    @Mock
+//    private WatchableRepo watchableRepo;
+//
+//    @InjectMocks
+//    private WatchableService watchableService;
+
+    WatchableRepo watchableRepo =  Mockito.mock(WatchableRepo.class);
+    WatchableService watchableService = new WatchableService(watchableRepo);
+
+    @Test
+    void getAll_ShouldReturnListOfSizeOne_ContainingGivenWatchable() {
+
+        // given
+        Watchable w1 = new Watchable(
+                "1",
+                "Interstellar",
+                List.of("Matthew McConaughey", "Anne Hathaway"),
+                "02:49",
+                List.of("Christopher Nolan"),
+                LocalDateTime.of(2014, 11, 7, 0, 0),
+                List.of("SciFi", "Drama"),
+                0,
+                12
+        );
+
+        List<Watchable> expected = List.of(w1);
+
+        when(watchableRepo.findAll()).thenReturn(expected);
+
+        // when
+        List<Watchable> result = watchableService.getAll();
+
+        // then
+        assertEquals(1, result.size());
+        assertEquals(w1, result.get(0));
+        assertEquals(expected, result);
+        verify(watchableRepo, times(1)).findAll();
+        verifyNoMoreInteractions(watchableRepo);
+    }
+}

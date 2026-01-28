d89655e089052e56c7f8af9cb45f3a83cbc41fc4
MaikeruDev <michaelrimbach51@gmail.com>
2026-01-28 16:10:11 +0100

    added streamable List to Movie Model

--- backend/src/main/java/org/example/backend/models/Movie.java
+++ backend/src/main/java/org/example/backend/models/Movie.java
@@ -3,6 +3,8 @@ package org.example.backend.models;
 import lombok.With;
 import org.springframework.data.annotation.Id;
 
+import java.util.List;
+
 @With
-public record Movie(@Id String id, String watchableID) {
+public record Movie(@Id String id, String watchableID, List<String> streamable) {
 }
